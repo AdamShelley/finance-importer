a243716588aa9a33a0c7f31de336bed7b67a8564
Adam Shelley <adam.shelley18@gmail.com>
2024-06-01 16:27:08 +0100

    Summary cards created

--- components/data-card.tsx
+++ components/data-card.tsx
@@ -1,6 +1,6 @@
 import { IconType } from "react-icons/lib";
 import { VariantProps, cva } from "class-variance-authority";
-import { cn } from "@/lib/utils";
+import { cn, formatCurrency, formatPercentage } from "@/lib/utils";
 import {
   Card,
   CardContent,
@@ -8,6 +8,8 @@ import {
   CardTitle,
   CardHeader,
 } from "./ui/card";
+import { CountUp } from "./count-up";
+import { Skeleton } from "./ui/skeleton";
 
 const boxVariant = cva("rounded-md p-3", {
   variants: {
@@ -54,7 +56,61 @@ export const DataCard = ({
   value = 0,
   variant,
   dateRange,
-  percentageChange,
+  percentageChange = 0,
 }: DataCardProps) => {
-  return <Card></Card>;
+  return (
+    <Card className="border-none drop-shadow-sm">
+      <CardHeader className="flex flex-row items-center justify-between">
+        <div className="space-y-2">
+          <CardTitle className="text-2xl line-clamp-1">{title}</CardTitle>
+          <CardDescription className="line-clamp-1">
+            {dateRange}
+          </CardDescription>
+        </div>
+        <div className={cn("shrink-0", boxVariant({ variant }))}>
+          <Icon className={cn(iconVariant({ variant }))} />
+        </div>
+      </CardHeader>
+      <CardContent>
+        <h1 className="font-bold text-2xl mb-2 line-clamp-1 break-all">
+          <CountUp
+            preserveValue
+            start={0}
+            end={value}
+            decimals={2}
+            decimalPlaces={2}
+            formattingFn={formatCurrency}
+          />
+        </h1>
+        <p
+          className={cn(
+            "text-muted-foreground text-sm line-clamp-1",
+            percentageChange > 0 && "text-emerald-500",
+            percentageChange < 0 && "text-rose-500"
+          )}
+        >
+          {formatPercentage(percentageChange, { addPrefix: true })} from last
+          period
+        </p>
+      </CardContent>
+    </Card>
+  );
+};
+
+export const DataCardLoading = () => {
+  return (
+    <Card className="border-none drop-shadow-sm h-[192px]">
+      <CardHeader className="flex flex-row items-center justify-between gap-x-4">
+        <div className="space-y-2">
+          <Skeleton className="w-24 h-6" />
+          <Skeleton className="w-40 h-4" />
+        </div>
+        <Skeleton className="size-12" />
+      </CardHeader>
+      <CardContent>
+        <Skeleton className="shrink-0 h-10 w-24 mb-2" />
+        <Skeleton className="shrink-0 h-4 w-40" />
+      </CardContent>
+    </Card>
+  );
 };
