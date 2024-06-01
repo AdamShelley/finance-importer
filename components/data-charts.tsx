1688c63f06ba19e8d03cbd768bda496679307145
Adam Shelley <adam.shelley18@gmail.com>
2024-06-01 16:27:08 +0100

    Adding data charts component

---
+++ components/data-charts.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useGetSummary } from "@/features/summary/api/use-get-summary";
+import { Keyboard } from "lucide-react";
+
+export const DataCharts = () => {
+  const { data, isLoading } = useGetSummary();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div className="grid">
+      <h1>Data Charts</h1>
+    </div>
+  );
+};
